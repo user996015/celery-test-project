a45c44b5cfdce71ffe01c3d8ee2af64614f95b62
Zach Borboa <zach.borboa@opensgi.com>
2012-05-29 13:05:03 -0700

    Update project to work with Django 1.4 default project layout

--- celery_test_project/urls.py
+++ celery_test_project/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^people/delay/(?P<person_id>\d+)/$', 'people.views.call_celery_delay', name='call-celery-delay'),
 )
